35417973202d6aa444c40b45bdd46a6444f53331
avilaparra.manuel <litoseether@gmail.com>
2023-08-25 11:21:30 +0300

    Anyways.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int A = 21;
         int Result = A;
         int B = 12;
-        int C = 31;
+        int C = 28;
 
         if (B > Result){
             Result = B;
@@ -13,6 +13,53 @@ public class Main {
         }
         System.out.println(Result);
 
+        ///Next Example
+
+        int X = 2;
+        int Y = 8;
+        int Result2 = 1;
+
+        while (Y > 0){
+            if (Y % 2 == 0){
+                Y = Y / 2;
+                X = X*X;
+            }else {
+                Y = Y-1;
+                Result2= Result2 * X;
+            }
+        }
+        System.out.println(Result2);
+
+        ///2 Homework
+
+        int Z, V, R, M;
+        Z = 5;
+        V = -7;
+        R = 0;
+
+
+        if (V < 0){
+            V = -V;
+            M = -1;
+        }else{
+            M = 1;
+        }
+        System.out.println(M + " " + V);
+
+        while (V > 0){
+
+            if ( V % 2 == 0){
+                Z = Z + Z;
+                V = V / 2;
+            }else {
+                R= R + Z;
+                V = V - 1;
+            }
+        }
+        if (M < 0){
+            R = -R;
+        }
+        System.out.println(R);
 
     }
 }
